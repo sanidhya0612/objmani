0a046e2f0100764580cb7f5606bbdaf7308867d0
sanidhya0612 <102963033+sanidhya0612@users.noreply.github.com>
2023-11-30 22:24:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+img="";
+status="";
+objects=[];
+
+function preload()
+{
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw()
+{
+
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
+}
