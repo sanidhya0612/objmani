fdd5b0a9881a90167b522f954e94de696055e44b
sanidhya0612 <102963033+sanidhya0612@users.noreply.github.com>
2023-12-05 21:59:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,21 @@ function setup()
 
 function draw()
 {
-
+    image(img,0,0,640,420);
+    
+    if(status!="")
+    {
+        for(i=0; i<objects.length; i++)
+        {
+            document.getElementByID("status").innerHTML = "Status : Object Detected";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label+""+percent+"%",objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000")
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 
 function modelLoaded()
